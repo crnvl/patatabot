dbf4ca36231fe6332bb71cfabfe083d9ed6c5925
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-07-09 20:57:54 +0200

    Create settings.java

---
+++ src/main/java/config/settings.java
@@ -0,0 +1,11 @@
+package config;
+
+public class settings {
+
+    public static String PREFIX = "pt!";
+
+    public static String VERSION = "1.0.0";
+
+    public static String TOKEN = System.getenv("BOT_TOKEN");
+
+}
