19e04b9876a6f37519939f22210637bf91b023ee
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2018-10-17 11:50:58 +0200

    Update Main.java

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         //Status
         builder.setStatus(OnlineStatus.ONLINE);
-        builder.setGame(Game.playing("Paladins: Champions of the Realm"));
+        builder.setGame(Game.playing("Minecraft auf: free4.freemc.host:31325"));
 
 
         //Listeners
