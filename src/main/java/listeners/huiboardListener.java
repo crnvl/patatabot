3b42b2534844c96a414864c162acdc5cffc60048
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-19 16:02:17 +0100

    Create huiboardListener.java

---
+++ src/main/java/listeners/huiboardListener.java
@@ -0,0 +1,42 @@
+package listeners;
+
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.awt.*;
+
+public class huiboardListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if (event.getReaction().getCount() > 4 && event.getReactionEmote().getId().contains("433969151422234635")) {
+            if(event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getEmbeds().size() == 0) {
+                event.getJDA().getTextChannelById("547417181068197898").sendMessage(
+                        new EmbedBuilder()
+                                .setColor(Color.PINK)
+                                .setTitle("Jump to Message", "https://discordapp.com/channels/" + event.getGuild().getId() + "/" + event.getChannel().getId() + "/" + event.getMessageId())
+                                .setDescription(event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getContentDisplay())
+                                .setAuthor(event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getAuthor().getName() + "#" + event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getAuthor().getDiscriminator(), event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getAuthor().getDefaultAvatarUrl())
+
+
+
+                                .build()).queue();
+            }else {
+                event.getJDA().getTextChannelById("547417181068197898").sendMessage(
+
+                        new EmbedBuilder()
+                                .setColor(Color.PINK)
+                                .setTitle("Jump to Message", "https://discordapp.com/channels/" + event.getGuild().getId() + "/" + event.getChannel().getId() + "/" + event.getMessageId())
+                                .setImage(event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getEmbeds().get(0).getImage().getUrl())
+                                .setDescription(event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getContentDisplay())
+                                .setAuthor(event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getAuthor().getName() + "#" + event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getAuthor().getDiscriminator(), event.getReaction().getChannel().getHistory().getMessageById(event.getMessageId()).getAuthor().getDefaultAvatarUrl())
+
+
+                                .build()).queue();
+            }
+
+        }
+    }
+}
