ef4039ada5aa156e096f371f2c0113dbca22a581
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-23 13:03:17 +0100

    Update Main.java

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,46 +1,46 @@
-package core;
+    package core;
 
 import config.settings;
-import listeners.huiboardListener;
-import net.dv8tion.jda.api.AccountType;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
+    import listeners.reactionRoleListener;
+    import net.dv8tion.jda.core.AccountType;
+    import net.dv8tion.jda.core.JDA;
+    import net.dv8tion.jda.core.JDABuilder;
+    import net.dv8tion.jda.core.OnlineStatus;
+    import net.dv8tion.jda.core.entities.Game;
 
-import javax.security.auth.login.LoginException;
-
-
-public class Main {
-
-    public static JDABuilder builder;
-
-    public static void main(String[] Args) throws LoginException, InterruptedException {
-        builder = new JDABuilder(AccountType.BOT);
-
-        //Important
-        builder.setToken(settings.TOKEN);
-        builder.setAutoReconnect(true);
-
-        //Status
-        builder.setStatus(OnlineStatus.ONLINE);
-        builder.setActivity(Activity.watching("#huiboard"));
-
-        //Listeners
-        builder.addEventListeners(new huiboardListener());
-        addCommands();
+    import javax.security.auth.login.LoginException;
 
 
-        try {
-            JDA jda = builder.build();
-        } catch (LoginException e) {
-            e.printStackTrace();
+    public class Main {
+
+        public static JDABuilder builder;
+
+        public static void main(String[] Args) throws LoginException, InterruptedException {
+            builder = new JDABuilder(AccountType.BOT);
+
+            //Important
+            builder.setToken(settings.TOKEN);
+            builder.setAutoReconnect(true);
+
+            //Status
+            builder.setStatus(OnlineStatus.ONLINE);
+            builder.setGame(Game.watching("#huiboard"));
+
+            //Listeners
+            builder.addEventListener(new reactionRoleListener());
+            addCommands();
+
+
+            try {
+                JDA jda = builder.build();
+            } catch (LoginException e) {
+                e.printStackTrace();
+            }
+
         }
 
-    }
+        public static void addCommands() {
 
-    public static void addCommands() {
-
-        //no commands added
+            //no commands added
+        }
     }
-}
