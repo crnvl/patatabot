4f7eeb6532f2b8f10f3b80fc875f9c91257f12a4
shinixsensei-dev <40737538+shinixsensei-dev@users.noreply.github.com>
2019-02-23 13:02:46 +0100

    Update huiboardListener.java

--- src/main/java/listeners/huiboardListener.java
+++ src/main/java/listeners/huiboardListener.java
@@ -1,12 +1,16 @@
 package listeners;
 
-import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.requests.RestAction;
+
+import javax.annotation.CheckReturnValue;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class huiboardListener extends ListenerAdapter {
-
-    @Override
-    public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
 
     }
 }
